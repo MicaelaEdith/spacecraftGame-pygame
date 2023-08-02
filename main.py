37a670c0a339a76af535131eaa991a7474ea1a35
micka tfc <romeromicaelaedith@gmail.com>
2023-08-02 18:51:41 -0300

    display - drawPlayer

---
+++ main.py
@@ -0,0 +1,44 @@
+import pygame
+import sys
+import os
+from playerController import Player
+
+pygame.init()
+
+os.environ['SDL_VIDEO_CENTERED']='1'
+info=pygame.display.Info()
+screenWidth, screenHeight= info.current_w, info.current_h
+white=(255,255,255)
+blue=(1,6,26)
+clock=pygame.time.Clock()
+
+xPosition=(screenWidth/2)-40
+yPosition=(screenHeight/100*80)+40
+
+if(screenWidth>1920 or screenHeight>1080):
+    screenWidth=1920
+    screenHeight=1080
+
+
+display = pygame.display.set_mode((screenWidth, screenHeight))
+pygame.display.set_caption("Juego")
+
+player=Player(xPosition,yPosition,screenHeight)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type==pygame.KEYDOWN:
+            if event.key==pygame.K_ESCAPE:
+                pygame.quit()
+                sys.exit()
+
+
+    display.fill(blue)
+    player.movePlayer(event)
+    player.drawPlayer(display)
+
+    pygame.display.flip()
+    clock.tick(60)
---
+++ playerController.py
@@ -0,0 +1,61 @@
+import pygame
+
+class Player():
+    def __init__(self, xPosition, yPosition,screenHeight) -> None:
+        self.xPosition=xPosition
+        self.yPosition=yPosition
+        self.yPositionInit=yPosition
+        self.point=0
+        self.health=5
+        self.animation=[]
+        self.animation.append(pygame.image.load("Assets/Player/spacecraft.png").convert())
+        self.animation.append(pygame.image.load("Assets/Player/spacecraft1.png").convert())
+        self.animation.append(pygame.image.load("Assets/Player/spacecraft2.png").convert())
+        self.animation[0].set_colorkey([1,6,26])
+        self.animation[1].set_colorkey([1,6,26])
+        self.animation[2].set_colorkey([1,6,26])
+        self.rect=self.animation[0].get_rect()
+        self.rect=self.animation[1].get_rect()
+        self.rect=self.animation[2].get_rect()
+        self.frame=0
+        self.keyDownCount=0
+        self.maxYPosition=screenHeight-(screenHeight/1.5)
+        self.slowArea=self.maxYPosition+(self.maxYPosition/2)
+     
+    def setPlayer(self, name):
+        self.name=name
+
+    def movePlayer(self, event):
+
+        if event.type==pygame.KEYDOWN:
+            self.keyDownCount+=1.5
+            if event.key==pygame.K_LEFT:
+                self.xPosition-=4
+            if event.key==pygame.K_RIGHT:
+                self.xPosition+=4
+            if event.key==pygame.K_UP and self.yPosition>self.maxYPosition:
+                if(self.yPosition<self.slowArea):
+                    self.yPosition-=2
+                else:
+                    self.yPosition-=4
+                
+            if event.key==pygame.K_DOWN:
+                self.yPosition+=2
+                
+            
+        if event.type==pygame.KEYUP:
+            self.keyDownCount=0
+            if event.key==pygame.K_UP:
+                if(self.yPosition<self.yPositionInit):
+                    self.yPosition+=1.5
+        
+        
+    def drawPlayer(self, display):
+        if(self.keyDownCount==0):
+            self.frame=0
+        elif(self.keyDownCount%2==0):
+            self.frame=1
+        else:
+            self.frame=2
+
+        display.blit(self.animation[self.frame], [self.xPosition, self.yPosition])
